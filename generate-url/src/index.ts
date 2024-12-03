22479576285e85648ea94f41badec7afa27d9fdc
ajayyai <ajayzyan100@gmail.com>
2024-12-03 09:10:27 +0530

    refactor: structure updated

--- generate-url/src/index.ts
+++ generate-url/src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import urlRoutes from '../routes/url';
+import urlRoutes from './routes/url';
 
 // Load environment variables
 dotenv.config();
